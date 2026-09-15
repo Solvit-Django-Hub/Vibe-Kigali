9cd5d6e868fddd955824dadfc01b657b6f53a6c0
Julienmj <julienmugisha1@gmail.com>
2026-09-15 12:23:31 +0200

    fix: correct MAILERS setting to EMAIL_BACKEND

--- vibe_kigali/settings.py
+++ vibe_kigali/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'categories',
+    'places',
+    'reviews',
+    'favorites',
+    'core',
+    'rest_framework',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -73,7 +82,7 @@ WSGI_APPLICATION = 'vibe_kigali.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+   'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
@@ -115,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Email
