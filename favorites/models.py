e3e52bb9c757b72dbda4ab5b2688d24336577166
Julienmj <julienmugisha1@gmail.com>
2026-09-15 12:25:41 +0200

    feat: add Favorite model for user-place bookmarks

---
+++ favorites/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.conf import settings
+from places.models import Place
+
+class Favorite(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='favorites')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='favorited_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'place')
+
+    def __str__(self):
+        return f"{self.user.username} ❤ {self.place.name}"
