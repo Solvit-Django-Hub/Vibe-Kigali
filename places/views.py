de233096d7a4d14ce5402038a499a56bceeccea6
Julienmj <julienmugisha1@gmail.com>
2026-09-17 16:26:30 +0200

    feat: add place list and detail API views

--- places/views.py
+++ places/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Place
+from .serializers import PlaceSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def place_list(request):
+    places = Place.objects.all()
+    serializer = PlaceSerializer(places, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def place_detail(request, pk):
+    place = get_object_or_404(Place, pk=pk)
+    serializer = PlaceSerializer(place)
+    return Response(serializer.data)
