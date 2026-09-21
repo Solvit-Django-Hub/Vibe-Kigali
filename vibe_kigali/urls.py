8f4ffa1c14092d423c233463b57e3d42fdd1ed39
Julienmj <julienmugisha1@gmail.com>
2026-09-21 22:11:17 +0200

    feat: update main URL configuration

--- vibe_kigali/urls.py
+++ vibe_kigali/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/places/', include('places.urls')),
+    path('api/categories/', include('categories.urls')),
+    path('api/reviews/', include('reviews.urls')),
+    path('api/favorites/', include('favorites.urls')),
 ]
