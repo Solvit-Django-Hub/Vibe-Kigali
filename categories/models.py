e021c08a1ff02af4a4e93f995b248bf83648b372
Julienmj <julienmugisha1@gmail.com>
2026-09-21 22:11:01 +0200

    feat: update categories model

--- categories/models.py
+++ categories/models.py
@@ -5,11 +5,17 @@ class Category(models.Model):
     description = models.TextField(blank=True)
     icon = models.CharField(max_length=50, blank=True)
     slug = models.SlugField(unique=True)
+    
+    def __str__(self):
+        return self.name
 
     
     
     
 class Activity(models.Model):
     name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
 
    
